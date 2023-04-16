48876f1a080a0b5e471b1d50acb33063c8e47fcc
Enrique Torreblanca <enriquetchat@gmail.com>
2023-04-16 14:58:08 -0700

    added router

--- pages/generate_code_page.js
+++ pages/generate_code_page.js
@@ -1,10 +1,18 @@
 import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const GenerateCodePage = () => {
+  const router = useRouter();
+  const { recordId } = router.query;
+
   useEffect(() => {
-   async function fetchData() {
+    async function fetchData() {
       try {
-        const recordId = 'your_record_id_here';
+        if (!recordId) {
+          console.error('Error: Record ID is missing from the URL.');
+          return;
+        }
+
         const response = await fetch(`/api/generate_code?recordId=${recordId}`);
         const data = await response.json();
         console.log(data);
@@ -13,8 +21,10 @@ const GenerateCodePage = () => {
       }
     }
 
-    fetchData();
-  }, []);
+    if (recordId) {
+      fetchData();
+    }
+  }, [recordId]);
 
   return (
     <div>
